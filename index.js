2d93edd87dfd86c45c6f3dfb95270851848bf8fc
Christian Dangl <boxblinkracer@gmx.net>
2023-06-02 18:25:41 +0200

    add dev tools and ci pipeline

--- index.js
+++ index.js
@@ -1,63 +1,46 @@
+/* eslint-disable no-undef */
 chrome.contextMenus.onClicked.addListener((info, tab) => {
+    const cardNumber = info.menuItemId.trim().replace(' ', '');
 
-        const cardNumber = info.menuItemId.trim().replace(' ', '');
+    /**
+     *
+     * @param text
+     */
+    function copyClipboard(text) {
+        const textarea = document.createElement('textarea');
+        textarea.value = text;
 
-        /**
-         *
-         * @param text
-         */
-        function insertText(text) {
-            document.activeElement.value = text;
-        }
+        document.body.appendChild(textarea);
+        textarea.select();
 
-        /**
-         *
-         * @param text
-         */
-        function copyClipboard(text) {
-            const textarea = document.createElement('textarea');
-            textarea.value = text;
+        document.execCommand('copy');
 
-            document.body.appendChild(textarea);
-            textarea.select();
-
-            document.execCommand('copy');
-
-            document.body.removeChild(textarea);
-        }
-
-        chrome.scripting.executeScript({
-            target: {tabId: tab.id},
-            func: copyClipboard,
-            args: [cardNumber],
-        });
+        document.body.removeChild(textarea);
     }
-);
 
+    /* eslint-disable no-undef */
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: copyClipboard,
+        args: [cardNumber],
+    });
+});
 
 chrome.runtime.onInstalled.addListener(function () {
     const cards = [
-        {cardNumber: '3782 822463 10005', name: 'American Express'},
-        {cardNumber: '2223 0000 1047 9399', name: 'Mastercard'},
-        {cardNumber: '4543 4740 0224 9996', name: 'VISA'},
-        {cardNumber: '4242 4242 4242 4242', name: 'Non 3DS'},
-    ]
-
-    let contexts = [
-        'page',
-        'selection',
-        'link',
-        'editable',
-        'image',
-        'video',
-        'audio'
+        { cardNumber: '3782 822463 10005', name: 'American Express' },
+        { cardNumber: '2223 0000 1047 9399', name: 'Mastercard' },
+        { cardNumber: '4543 4740 0224 9996', name: 'VISA' },
+        { cardNumber: '4242 4242 4242 4242', name: 'Non 3DS' },
     ];
+
+    const contexts = ['page', 'selection', 'link', 'editable', 'image', 'video', 'audio'];
 
     chrome.contextMenus.create({
         id: 'mollie',
         title: 'Mollie Payments',
         type: 'normal',
-        contexts: contexts
+        contexts: contexts,
     });
 
     chrome.contextMenus.create({
@@ -65,17 +48,16 @@ chrome.runtime.onInstalled.addListener(function () {
         title: 'Test Cards (copy to clipboard)',
         parentId: 'mollie',
         type: 'normal',
-        contexts: contexts
+        contexts: contexts,
     });
 
-    cards.forEach(card => {
+    cards.forEach((card) => {
         chrome.contextMenus.create({
             id: card.cardNumber,
             title: `${card.name} (${card.cardNumber})`,
             parentId: 'testcards',
             type: 'normal',
-            contexts: contexts
+            contexts: contexts,
         });
     });
-
 });
